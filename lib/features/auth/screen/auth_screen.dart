70f69ecf49e0d29368f19f7ab35c2b760bf982ff
bill7maxmaye <bilenmehalek7@gmail.com>
2024-02-16 09:08:08 +0300

    complete radio button

--- lib/features/auth/screen/auth_screen.dart
+++ lib/features/auth/screen/auth_screen.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:merkato/constants/global_variables.dart';
+
+enum Auth{
+  signin,
+  signup,
+}
+
+
 class AuthScreen extends StatefulWidget {
-  const AuthScreen({Key? key}) : super(key: key);
   static const String routeName =  "/auth-screen";
+  const AuthScreen({Key? key}) : super(key: key);
+
   @override
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("welcome auth page"),),
+    return  Scaffold(
+      backgroundColor: GlobalVariables.greyBackgroundColor,
+      body: SafeArea(child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(children: [
+           const Text("WELCOME",  style: TextStyle(fontSize: 22, fontWeight:FontWeight.w500 ),),
+          ListTile(
+            title:  Text("Create Account", style: TextStyle(fontWeight: FontWeight.bold),),
+            leading: Radio(activeColor: GlobalVariables.secondaryColor,
+                value: Auth.signup,
+                groupValue: _auth,
+                onChanged: (Auth ? val){
+                    setState(() {
+                      _auth = val!;
+                    });
+                }),
+          ),
+          ListTile(
+            title:  Text("Sign-In", style: TextStyle(fontWeight: FontWeight.bold),),
+            leading: Radio(activeColor: GlobalVariables.secondaryColor,
+                value: Auth.signin,
+                groupValue: _auth,
+                onChanged: (Auth ? val){
+                  setState(() {
+                    _auth = val!;
+                  });
+                }),
+          )
+
+        ],),
+      ))
     );
   }
 }
