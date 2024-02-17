f88a724ef6a6aa8ce7550dbf9da3f451283caa99
bill7maxmaye <bilenmehalek7@gmail.com>
2024-02-17 09:57:02 +0300

    complete the signup authentication

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
+//IMORT FROM PACKAGES
 const express = require ('express');
 
-const app = express();
+//IMPORT FROM FILES
+//this just imports it for use
+const authRouter = require("./routes/auth")
+
+//INITILIZATIONS
 PORT = 3000
+const app = express();
+
+
+//MIDDLEWARE
+//this now makes it functional that the server listening on port 3000, when any request to the specified get request comes it directs it to the authRouter
+app.use(authRouter);
+
+
+
 
 app.listen(PORT,"0.0.0.0",  ()=>{
 console.log(`started server at port ${PORT}`)})
