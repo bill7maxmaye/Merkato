eee76971b9e25c69aef1a58047393f74642f12d3
bill7maxmaye <bilenmehalek7@gmail.com>
2024-02-24 21:59:15 +0300

    create home appbar

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:merkato/constants/global_variables.dart';
 import 'package:merkato/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -15,8 +16,71 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(60),
+            child: AppBar(
+              flexibleSpace: Container(
+                  decoration: const BoxDecoration(
+                      gradient: GlobalVariables.appBarGradient)),
+              title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                          height: 42,
+                          margin: const EdgeInsets.only(
+                            left: 15,
+                          ),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(7),
+                            elevation: 1,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                prefixIcon: InkWell(
+                                  onTap: () {},
+                                  child: const Padding(
+                                    padding: EdgeInsets.only(left: 6),
+                                    child: Icon(
+                                      Icons.search,
+                                      color: Colors.black,
+                                      size: 23,
+                                    ),
+                                  ),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                hintText: 'Search for products',
+                                hintStyle: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                contentPadding: const EdgeInsets.only(top: 10),
+                                border: const OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(7),
+                                    ),
+                                    borderSide: BorderSide.none),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(7)),
+                                    borderSide: BorderSide(
+                                        color: Colors.black38, width: 1)),
+                              ),
+                            ),
+                          )),
+                    ),
+                    Container(
+                      color: Colors.transparent,
+                      height: 43,
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      child:
+                          const Icon(Icons.mic, color: Colors.black, size: 25),
+                    )
+                  ]),
+            )),
         body: Center(
-      child: Text(user.toJson()),
-    ));
+          child: Text(user.toJson()),
+        ));
   }
 }
