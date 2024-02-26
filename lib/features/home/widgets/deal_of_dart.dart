8e540b9b6bcf6602e7166c5eccc2c74442346a7d
bill7maxmaye <bilenmehalek7@gmail.com>
2024-02-27 01:02:07 +0300

    added home page deal of the day

--- lib/features/home/widgets/deal_of_dart.dart
+++ lib/features/home/widgets/deal_of_dart.dart
@@ -10,13 +10,21 @@ class DealOfDay extends StatefulWidget {
 class _DealOfDayState extends State<DealOfDay> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.topLeft,
-      padding: EdgeInsets.only(top: 15, left: 10),
-      child: Text(
-        "Deal of the day",
-        style: TextStyle(fontSize: 20),
-      ),
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.only(top: 15, left: 10),
+          child: Text(
+            "Deal of the day",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        Image.network(
+            "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvbXB1dGVyfGVufDB8fDB8fHww",
+            height: 232,
+            fit: BoxFit.fitHeight),
+      ],
     );
   }
 }
