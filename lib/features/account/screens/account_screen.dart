68d5930ea8ad81f3232b512945d88b85fd45b119
bill7maxmaye <bilenmehalek7@gmail.com>
2024-02-26 10:31:48 +0300

    create home appbar

--- lib/features/account/screens/account_screen.dart
+++ lib/features/account/screens/account_screen.dart
@@ -39,7 +39,8 @@ class AccountScreen extends StatelessWidget {
                     ]),
                   ),
                 ]),
-          )),
+          )
+          ),
       body: Column(
         children: [
           BelowAppBar(),
