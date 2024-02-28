1145f590775e74b69d31b3818659f4a92c4affde
bill7maxmaye <bilenmehalek7@gmail.com>
2024-02-28 15:57:16 +0300

    created admin page

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:merkato/constants/global_variables.dart';
 
@@ -16,6 +17,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
       appBar: PreferredSize(
           preferredSize: const Size.fromHeight(50),
           child: AppBar(
+            centerTitle: true,
             flexibleSpace: Container(
                 decoration: const BoxDecoration(
                     gradient: GlobalVariables.appBarGradient)),
@@ -24,6 +26,42 @@ class _AddProductScreenState extends State<AddProductScreen> {
               style: TextStyle(color: Colors.black),
             ),
           )),
+      body: SingleChildScrollView(
+          child: Form(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.folder_open,
+                            size: 40,
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            'Select Product Image',
+                            style: TextStyle(
+                                color: Colors.grey.shade400, fontSize: 15),
+                          )
+                        ],
+                      )))
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
